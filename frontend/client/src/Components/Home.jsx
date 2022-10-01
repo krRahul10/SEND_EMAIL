afa0e3295dac0a16c50786a03163207329e17ba3
Rahul <rahul10kr7@gmail.com>
2022-10-01 09:13:26 +0530

    email sent

--- frontend/client/src/Components/Home.jsx
+++ frontend/client/src/Components/Home.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { useState } from "react";
+import Alert from 'react-bootstrap/Alert';
 
 const Home = () => {
   const [email, setEmail] = useState("");
+  const [show, setShow] = useState(false);
   // console.log(email);
 
   const sendEmail = async(e) => {
@@ -18,12 +20,30 @@ const Home = () => {
         body:JSON.stringify({email})
     })
 
-    console.log(res)
+    const data = await res.json()
+
+    if(data.status ===401 || !data){
+      console.log("error")
+    }else{
+    console.log("Email Sent Successfully");
+    setShow(true)
+    setEmail("")
+    }
 
   };
 
   return (
     <div>
+      {
+        show ? <Alert variant="warning" onClose={() => setShow(false)} dismissible>
+        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+        <p>
+          Change this and that and try again. Duis mollis, est non commodo
+          luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+          Cras mattis consectetur purus sit amet fermentum.
+        </p>
+      </Alert> :""
+      }
       <div className="container mt-2">
         <div className="d-flex justify-content-center">
           <h2>Send Email With React & NodeJs</h2>
